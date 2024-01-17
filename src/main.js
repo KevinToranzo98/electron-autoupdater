bc4daf3671c58f20d29c9c0803c15b0e3a93d5e9
Kevin Toranzo <ktoranzo@boxcustodia.com>
2024-01-17 16:34:27 -0300

    Pequeños cambios

--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
-const { windowConfig } = require('./utils/windowConfig');
 const { app, BrowserWindow, Menu, Tray } = require('electron');
+const { autoUpdater, AppUpdater } = require('electron-updater');
+const { windowConfig } = require('./utils/windowConfig');
 const path = require('path');
 const { stopServers } = require('./services');
 
+let win;
+
+//Basic flags
+autoUpdater.autoDownload = false;
+autoUpdater.autoInstallOnAppQuit = true;
+
 function createWindow() {
-  const win = new BrowserWindow(windowConfig);
+  win = new BrowserWindow(windowConfig);
   win.loadFile(path.join(__dirname, 'main.html'));
   win.once('ready-to-show', () => {
     win.show(); // Muestra la ventana cuando está lista
     setTimeout(() => {
       win.hide();
+      autoUpdater.checkForUpdates();
+      console.log(`Checking for updates. Current version ${app.getVersion()}`);
     }, 2000);
     win.isMinimized();
   });
@@ -31,6 +40,32 @@ function createWindow() {
   // Establecer el menú contextual de este icono
   appTray.setContextMenu(contextMenu);
 }
+
+/*New Update Available*/
+autoUpdater.on('update-available', (info) => {
+  curWindow.showMessage(
+    `Update available. Current version ${app.getVersion()}`
+  );
+  let pth = autoUpdater.downloadUpdate();
+  curWindow.showMessage(pth);
+});
+
+autoUpdater.on('update-not-available', (info) => {
+  curWindow.showMessage(
+    `No update available. Current version ${app.getVersion()}`
+  );
+});
+
+/*Download Completion Message*/
+autoUpdater.on('update-downloaded', (info) => {
+  curWindow.showMessage(
+    `Update downloaded. Current version ${app.getVersion()}`
+  );
+});
+
+autoUpdater.on('error', (info) => {
+  curWindow.showMessage(info);
+});
 
 app.whenReady().then(createWindow);
 app.on('window-all-closed', () => {
